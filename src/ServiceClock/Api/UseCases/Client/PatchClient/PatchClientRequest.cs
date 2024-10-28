54c3586c359d4573e21ff6c00f2be16e7e301d1f
Gabriel <gabrielvictor159487@gmail.com>
2024-10-28 09:25:27 -0300

    remove JsonProperty patch

--- src/ServiceClock/Api/UseCases/Client/PatchClient/PatchClientRequest.cs
+++ src/ServiceClock/Api/UseCases/Client/PatchClient/PatchClientRequest.cs
@@ -4,27 +4,16 @@ namespace ServiceClock_BackEnd.Api.UseCases.Client.PatchClient;
 
 public class PatchClientRequest
 {
-    [JsonProperty("Name")]
     public string Name { get; set; } = "";
-    [JsonProperty("Password")]
     public string Password { get; set; } = "";
-    [JsonProperty("Email")]
     public string Email { get; set; } = "";
-    [JsonProperty("PhoneNumber")]
     public string PhoneNumber { get; set; } = "";
-    [JsonProperty("Address")]
     public string Address { get; set; } = "";
-    [JsonProperty("Image")]
     public string Image { get; set; } = "";
-    [JsonProperty("ImageName")]
     public string ImageName { get; set; } = "";
-    [JsonProperty("City")]
     public string City { get; set; } = "";
-    [JsonProperty("State")]
     public string State { get; set; } = "";
-    [JsonProperty("PostalCode")]
     public string PostalCode { get; set; } = "";
-    [JsonProperty("BirthDate")]
     public DateTime BirthDate { get; set; } = DateTime.MinValue;
 }
 
--- src/ServiceClock/Api/UseCases/Company/PatchCompany/PatchCompanyRequest.cs
+++ src/ServiceClock/Api/UseCases/Company/PatchCompany/PatchCompanyRequest.cs
@@ -5,25 +5,15 @@ namespace ServiceClock_BackEnd.Api.UseCases.Company.PatchCompany;
 
 public class PatchCompanyRequest
 {
-    [JsonProperty("Name")]
     public string Name { get; set; } = "";
-    [JsonProperty("Password")]
     public string Password { get; set; } = "";
-    [JsonProperty("RegistrationNumber")]
     public string RegistrationNumber { get; set; } = "";
-    [JsonProperty("Address")]
     public string Address { get; set; } = "";
-    [JsonProperty("Image")]
     public string Image { get; set; } = "";
-    [JsonProperty("ImageName")]
     public string ImageName { get; set; } = "";
-    [JsonProperty("City")]
     public string City { get; set; } = "";
-    [JsonProperty("State")]
     public string State { get; set; } = "";
-    [JsonProperty("PostalCode")]
     public string PostalCode { get; set; } = "";
-    [JsonProperty("PhoneNumber")]
     public string PhoneNumber { get; set; } = "";
 }
 
